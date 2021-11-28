2171f5107802ec684e102437469ff06a12361cc3
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2021-11-28 17:21:25 +0200

    Add files via upload

---
+++ Python OOP/3. Inheritance/Exercise/4. Need for Speed/project/vehicle.py
@@ -0,0 +1,14 @@
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION = 1.25
+
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel = fuel
+        self.horse_power = horse_power
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+
+    def drive(self, kilometers):
+        spend_fuel = kilometers * self.fuel_consumption
+        if spend_fuel <= self.fuel:
+            self.fuel -= spend_fuel
+
+
